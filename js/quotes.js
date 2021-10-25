d5304716e053e5fa12a7fefb8eefed828fdcded3
spedi123 <peteran8604@gmail.com>
2021-10-25 00:42:34 -0700

    made quotes

--- js/quotes.js
+++ js/quotes.js
@@ -1,42 +1,51 @@
 const quotes = [
     {
-        quotes: "Nobody can make you feel inferior without your permission",
+        quotes: "Nobody can make you feel inferior without your permission.",
         author: "Eleanor Roosevelt"
     },
     {
-        quotes: "You can never plan the future by the past",
+        quote: "You can never plan the future by the past.",
         author: "Edmund Burke"
     },
     {
-        quotes: "Take into account that great love and great achievements involve great risk",
+        quote: "Take into account that great love and great achievements involve great risk.",
         author: "Dalai Lama"
     },
     {
-        quotes: "He that respects himself is safe from others",
+        quote: "He that respects himself is safe from others.",
         author: "Henry Wadsworth Longfellow"
     },
     {
-        quotes: "Common sense is genius dressed in its working clothes",
+        quote: "Common sense is genius dressed in its working clothes.",
         author: "Ralph Waldo Emerson"
     },
     {
-        quotes: "In three words I can sum up everything I’ve learned about life: it goes on",
+        quote: "In three words I can sum up everything I’ve learned about life: it goes on.",
         author: "Robert Frost"
     },
     {
-        quotes: "It is the province of knowledge to speak, and it is the privilege of wisdom to listen",
+        quote: "It is the province of knowledge to speak, and it is the privilege of wisdom to listen.",
         author: "Oliver Wendell Holmes Sr."
     },
     {
-        quotes: "Don’t take life too seriously. You’ll never get out of it alive",
+        quote: "Don’t take life too seriously. You’ll never get out of it alive.",
         author: "Elbert Hubbard"
     },
     {
-        quotes: "Nothing that you have not given away will ever be really yours",
+        quote: "Nothing that you have not given away will ever be really yours.",
         author: "C. S. Lewis"
     },
     {
-        quotes: "The two most important days in your life are the day you are born and the day you find out why",
+        quote: "The two most important days in your life are the day you are born and the day you find out why.",
         author: "Mark Twain"
     }
 ]
+
+const quote = document.querySelector("#quote span:first-child")
+const author = document.querySelector("#quote span:last-child")
+
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)]
+
+quote.innerText = todaysQuote.quote
+author.innerText = todaysQuote.author
