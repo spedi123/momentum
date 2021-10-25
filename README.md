9d735cb185cf4c63e8d285a3784df49914db908d
spedi123 <peteran8604@gmail.com>
2021-10-24 23:35:22 -0700

    made a clock

---
+++ README.md
@@ -0,0 +1,2 @@
+Momentum 
+java scrpit practice
---
+++ js/quotes.js
@@ -0,0 +1,42 @@
+const quotes = [
+    {
+        quotes: "Nobody can make you feel inferior without your permission",
+        author: "Eleanor Roosevelt"
+    },
+    {
+        quotes: "You can never plan the future by the past",
+        author: "Edmund Burke"
+    },
+    {
+        quotes: "Take into account that great love and great achievements involve great risk",
+        author: "Dalai Lama"
+    },
+    {
+        quotes: "He that respects himself is safe from others",
+        author: "Henry Wadsworth Longfellow"
+    },
+    {
+        quotes: "Common sense is genius dressed in its working clothes",
+        author: "Ralph Waldo Emerson"
+    },
+    {
+        quotes: "In three words I can sum up everything I’ve learned about life: it goes on",
+        author: "Robert Frost"
+    },
+    {
+        quotes: "It is the province of knowledge to speak, and it is the privilege of wisdom to listen",
+        author: "Oliver Wendell Holmes Sr."
+    },
+    {
+        quotes: "Don’t take life too seriously. You’ll never get out of it alive",
+        author: "Elbert Hubbard"
+    },
+    {
+        quotes: "Nothing that you have not given away will ever be really yours",
+        author: "C. S. Lewis"
+    },
+    {
+        quotes: "The two most important days in your life are the day you are born and the day you find out why",
+        author: "Mark Twain"
+    }
+]
