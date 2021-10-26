78f89b5fc7b809858640fd7d29ac521e4a96b2c5
spedi123 <peteran8604@gmail.com>
2021-10-25 22:36:39 -0700

    finished momentum app

--- js/background.js
+++ js/background.js
@@ -9,4 +9,5 @@ const chosenImage = images[Math.floor(Math.random() * images.length)]
 const bgImage = document.createElement("img")
 
 bgImage.src = `img/${chosenImage}`
+bgImage.classNmae = "bgImage"
 document.body.appendChild(bgImage)
--- js/quotes.js
+++ js/quotes.js
@@ -44,7 +44,6 @@ const quotes = [
 const quote = document.querySelector("#quote span:first-child")
 const author = document.querySelector("#quote span:last-child")
 
-
 const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)]
 
 quote.innerText = todaysQuote.quote
